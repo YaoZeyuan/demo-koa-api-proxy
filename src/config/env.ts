613521337ceaa20f7ae4ba747f5695ebe2255343
yaozeyuan001 <yaozeyuan001@ke.com>
2021-10-26 22:12:58 +0800

    feat: 添加默认环境变量

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,2 @@
-export default (process.env.NODE_ENV ?? 'dev') as "local" | "dev" | "test" | "pre" | "prod"
+let node_env = process.env.NODE_ENV ?? "dev"
+export default node_env as "local" | "dev" | "test" | "pre" | "prod"
